fca72354007fde7f9c6f3e3102010a59d29f51af
Agrim9 <agrim9gupta@gmail.com>
2017-02-04 15:20:54 +0530

    Arm Commands

--- build/roboclaw_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/utkarsh/rover_ws/src/roboclaw_node
-# Build directory: /home/utkarsh/rover_ws/build/roboclaw_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/rover_mobility/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/utkarsh/rover_ws/src/rover_mobility
-# Build directory: /home/utkarsh/rover_ws/build/rover_mobility
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
